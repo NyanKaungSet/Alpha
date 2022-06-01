1b942dab41f14d94731af0455bc669f808574a76
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-06-01 18:21:43 +0630

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,7 @@ var upDownSwitch = document.querySelector('#up-down-switch');
 upDownSwitch.style="font-size:30px;transform: rotate(180deg);";
 var lgDiv = document.querySelector('#logoDiv');
 var isPageUp = true;
-/*upDownSwitch.onclick = () => {
+upDownSwitch.onclick = () => {
     if(isPageUp){
         lgDiv.style="display:none;";
         upDownSwitch.style="font-size:30px;transform: rotate(0deg);";
@@ -12,4 +12,19 @@ var isPageUp = true;
         upDownSwitch.style="font-size:30px;transform: rotate(180deg);";
         isPageUp = true;
     }
-}*/
+}
+
+
+var prevScrollpos = window.pageYOffset;
+var ion = document.getElementById('up-down-switch');
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+if (prevScrollpos > currentScrollPos) {
+    document.getElementById("topbar").style.top = "0px";
+    ion.style.display = "block";
+} else {
+    document.getElementById("topbar").style.top = "-150px";
+    ion.style.display = "none";
+}
+prevScrollpos = currentScrollPos;
+}
