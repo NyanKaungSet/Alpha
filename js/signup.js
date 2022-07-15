e69038b0638605050ec723279663f5a5786b4eda
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-07-15 12:25:28 +0630

    Add files via upload

---
+++ js/signup.js
@@ -0,0 +1,45 @@
+const unSignup = document.querySelector('#un-signup');
+const eSignup = document.querySelector('#em-signup');
+const pwdSignup = document.querySelector('#pwd-signup');
+const signupBtn = document.querySelector('#signup-btn');
+const bySignup = document.querySelector('#by-signup');
+
+signupBtn.onclick = () => {
+    if(eSignup.value){
+        if(pwdSignup.value){
+            if(unSignup.value){
+                if(bySignup.value){
+                    signup(eSignup.value, pwdSignup.value)
+                
+                }else{
+                    alert("require birthyear.")
+                }
+            }else{
+                alert("require nickname.")
+            }
+        }else{
+            alert("require password.")
+        }
+    }else{
+        alert("require email address.")
+    }
+}
+var UID
+
+auth.onAuthStateChanged(user => {
+    firebase.database().ref().child('users').child(firebase.auth().getUid()).get().then(s=>{
+        if(s.exists()){
+            location.href="dashboard.html"
+        }else{
+            firebase.database().ref().child('users').child(user.uid).set({
+                name:unSignup.value,
+                birthYear:bySignup.value,
+                email:eSignup.value,
+                profilePicture:"default",
+            }).then(a=>{
+                alert("Sign Up successful!")
+                location.href="dashboard.html"
+            })
+        }
+    })
+})
